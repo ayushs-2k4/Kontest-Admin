0b0a4c5d5b579dad6b3ba4defa74330ed8c8594b
Ayush Singhal <ayushsinghals02@gmail.com>
2024-05-14 16:00:59 +0530

    Initial Commit

---
+++ Kontest Admin/Kontest_AdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kontest_AdminApp.swift
+//  Kontest Admin
+//
+//  Created by Ayush Singhal on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Kontest_AdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
